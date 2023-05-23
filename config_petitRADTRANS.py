814235cfe38cc7da4096e30d9e6aa281964c75f0
JHayoz <jeanhayoz94@gmail.com>
2023-05-23 18:31:00 +0200

    Added petitRADTRANS config file to define the names of the opacities

---
+++ config_petitRADTRANS.py
@@ -0,0 +1,2 @@
+POOR_MANS_ABUND_LBL = ['H2O_main_iso','CO_main_iso','CH4_main_iso','CO2_main_iso','H2S_main_iso','NH3_main_iso','FeH_main_iso','HCN_main_iso','TiO_all_iso','PH3_main_iso','K', 'VO','Na','He','H2']
+POOR_MANS_ABUND_CK = ['H2O','CO','CH4','CO2','H2S','NH3_HITRAN','FeH_Chubb','HCN','TiO','PH3','K', 'VO','Na','He','H2']
